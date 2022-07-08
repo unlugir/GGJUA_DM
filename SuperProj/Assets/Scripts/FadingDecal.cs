1a20881ca2fe54f4a80ca38879572924cdc84965
unlugir <treedimober@gmail.com>
2022-07-08 22:56:28 +0300

    Add decal prefab, world item prefab, barker prefab

---
+++ SuperProj/Assets/Scripts/FadingDecal.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class FadingDecal : MonoBehaviour
+{
+    [SerializeField] bool isVisible;
+    Color initialColor;
+    SpriteRenderer sprite;
+    private void Start()
+    {
+        sprite = this.GetComponent<SpriteRenderer>();
+        initialColor = sprite.color;
+        if (!isVisible)
+        {
+            sprite.color = Color.clear;
+            gameObject.SetActive(false);
+        }
+
+    }
+    public void FadeIn()
+    {
+        if (isVisible) return;
+        isVisible = true;
+        sprite.DOColor(initialColor, 1f).onComplete += () => gameObject.SetActive(true);
+    }
+    public void FadeOut()
+    {
+        if (!isVisible) return;
+        isVisible = false;
+        sprite.DOColor(Color.clear, 1f).onComplete += () => gameObject.SetActive(false);
+    }
+}
